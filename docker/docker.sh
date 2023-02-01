93d8054c9f72a014632d7e54461a788f53a785d1
wakewakame <0214.taiki@gmail.com>
2023-02-01 21:04:04 +0900

    arm系のPCでもビルドできるように修正

--- docker/docker.sh
+++ docker/docker.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "$(readlink "$0" || echo "$0")")" && pwd)"
 docker build -t wasm_mvg $SCRIPT_DIR
 docker run \
   -it --rm \
+  -platform linux/x86_64 \
   --mount type=bind,source="$(realpath "${SCRIPT_DIR}/../")",target=/home/user/app \
   wasm_mvg
 
