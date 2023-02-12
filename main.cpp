e8938c4f11a38a0321231f32dbc247de54bd9ac4
wakewakame <0214.taiki@gmail.com>
2023-02-13 05:21:30 +0900

    5点アルゴリズムのラッパー作成

--- main.cpp
+++ main.cpp
@@ -1,91 +1,78 @@
 #include <iostream>
-#include <chrono>
-#include <string.h>
-#include "examples/main_SfMInit_ImageListing.h"
-#include "examples/main_ComputeFeatures.h"
-#include "examples/main_ComputeMatches.h"
-#include "examples/main_GeometricFilter.h"
-#include "examples/main_SfM.h"
+#include "openMVG/sfm/sfm.hpp"
+#include "openMVG/cameras/cameras.hpp"
 
-#ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
-#define EXPORT extern "C" EMSCRIPTEN_KEEPALIVE
-#else
-#define EXPORT extern "C"
-#endif
+#include <emscripten/bind.h>
 
-EXPORT int main(int argc, char **argv) {
-	const std::string path_root{argv[1]};
+openMVG::Mat valToMat(const emscripten::val &val) {
+	const size_t cols = val["length"].as<unsigned int>();
+	const size_t rows = val[0]["length"].as<unsigned int>();
+	openMVG::Mat ret{rows, cols};
+	for(size_t col = 0; col < cols; col++) {
+		for(size_t row = 0; row < rows; row++) {
+			ret(row, col) = val[col][row].as<double>();
+		}
+	}
+	return ret;
+}
+emscripten::val matToVal(const openMVG::Mat &mat) {
+	emscripten::val ret = emscripten::val::array();
+	for(size_t col = 0; col < mat.cols(); col++) {
+		emscripten::val row_val = emscripten::val::array();
+		for(size_t row = 0; row < mat.rows(); row++) {
+			row_val.call<emscripten::val>("push", emscripten::val(mat(row, col)));
+		}
+		ret.call<emscripten::val>("push", row_val);
+	}
+	return ret;
+}
 
-	const std::string path_images = path_root + "/images";
-	const std::string path_matches = path_root + "/matches";
-	char* opts1[9] = {
-		strdup("./main_SfMInit_ImageListing"),
-		strdup("-i"), strdup(path_images.c_str()),
-		strdup("-o"), strdup(path_matches.c_str()),
-		strdup("-c"), strdup("3"),
-		strdup("-f"), strdup("3398")
+emscripten::val getRelativePose(
+	const emscripten::val &img1_cam, const emscripten::val &img1_points,
+	const emscripten::val &img2_cam, const emscripten::val &img2_points
+) {
+	openMVG::cameras::Pinhole_Intrinsic_Radial_K3 img1_cam_{
+		img1_cam["w"].as<int>(),
+		img1_cam["h"].as<int>(),
+		img1_cam["focal"].as<double>(),
+		img1_cam["ppx"].as<double>(),
+		img1_cam["ppy"].as<double>(),
+		img1_cam["k1"].as<double>(),
+		img1_cam["k2"].as<double>(),
+		img1_cam["k3"].as<double>()
 	};
-	main_SfMInit_ImageListing(9, opts1);
-
-	const std::chrono::system_clock::time_point time0 = std::chrono::system_clock::now();
-
-	const std::string path_matches_sfm_data = path_matches + "/sfm_data.json";
-	char* opts2[9] = {
-		strdup("./main_ComputeFeatures"),
-		strdup("-i"), strdup(path_matches_sfm_data.c_str()),
-		strdup("-o"), strdup(path_matches.c_str()),
-		strdup("-m"), strdup("AKAZE_FLOAT"),
-		strdup("-f"), strdup("1")
+	openMVG::cameras::Pinhole_Intrinsic_Radial_K3 img2_cam_{
+		img2_cam["w"].as<int>(),
+		img2_cam["h"].as<int>(),
+		img2_cam["focal"].as<double>(),
+		img2_cam["ppx"].as<double>(),
+		img2_cam["ppy"].as<double>(),
+		img2_cam["k1"].as<double>(),
+		img2_cam["k2"].as<double>(),
+		img2_cam["k3"].as<double>()
 	};
-	main_ComputeFeatures(9, opts2);
 
-	const std::chrono::system_clock::time_point time1 = std::chrono::system_clock::now();
+	openMVG::Mat img1_points_ = valToMat(img1_points);
+	openMVG::Mat img2_points_ = valToMat(img2_points);
 
-	const std::string path_matches_matches_putative = path_matches + "/matches.putative.bin";
-	char* opts3[9] = {
-		strdup("./main_ComputeMatches"),
-		strdup("-i"), strdup(path_matches_sfm_data.c_str()),
-		strdup("-o"), strdup(path_matches_matches_putative.c_str()),
-		strdup("-f"), strdup("1"),
-		strdup("-n"), strdup("ANNL2")
-	};
-	main_ComputeMatches(9, opts3);
+	openMVG::sfm::RelativePose_Info relativePose_info;
+	if(!robustRelativePose(
+		&img1_cam_, &img2_cam_, img1_points_, img2_points_, relativePose_info,
+		{img1_cam_.w(), img1_cam_.h()}, {img2_cam_.w(), img2_cam_.h()},
+		4096
+	)) {
+		return emscripten::val::null();
+	}
 
-	const std::chrono::system_clock::time_point time2 = std::chrono::system_clock::now();
+	const emscripten::val rotation = matToVal(relativePose_info.relativePose.rotation());
+	const emscripten::val center = matToVal(relativePose_info.relativePose.center());
+	emscripten::val result = emscripten::val::object();
+	result.set("rotation", rotation);
+	result.set("center", center);
+	return result;
+}
 
-	const std::string path_matches_matches_f = path_matches + "/matches.f.bin";
-	char* opts4[9] = {
-		strdup("./main_GeometricFilter"),
-		strdup("-i"), strdup(path_matches_sfm_data.c_str()),
-		strdup("-m"), strdup(path_matches_matches_putative.c_str()),
-		strdup("-g"), strdup("f"),
-		strdup("-o"), strdup(path_matches_matches_f.c_str())
-	};
-	main_GeometricFilter(9, opts4);
-
-	const std::chrono::system_clock::time_point time3 = std::chrono::system_clock::now();
-
-	const std::string path_sparse_model = path_root + "/sparse_model";
-	char* opts5[9] = {
-		strdup("./main_SfM"),
-		strdup("-s"), strdup("INCREMENTAL"),
-		strdup("-i"), strdup(path_matches_sfm_data.c_str()),
-		strdup("-m"), strdup(path_matches.c_str()),
-		strdup("-o"), strdup(path_sparse_model.c_str())
-	};
-	main_SfM(9, opts5);
-
-	const std::chrono::system_clock::time_point time4 = std::chrono::system_clock::now();
-
-	std::cout
-		<< "ComputeFeatures: " << std::chrono::duration_cast<std::chrono::seconds>(time1 - time0).count() << " s\n"
-		<< "ComputeMatches : " << std::chrono::duration_cast<std::chrono::seconds>(time2 - time1).count() << " s\n"
-		<< "GeometricFilter: " << std::chrono::duration_cast<std::chrono::seconds>(time3 - time2).count() << " s\n"
-		<< "SfM            : " << std::chrono::duration_cast<std::chrono::seconds>(time4 - time3).count() << " s\n"
-		<< "========================\n"
-		<< "Total          : " << std::chrono::duration_cast<std::chrono::seconds>(time4 - time0).count() << " s"
-		<< std::endl;
-
-	return 0;
+EMSCRIPTEN_BINDINGS(my_class_example) {
+	emscripten::function("getRelativePose", &getRelativePose);
 }
